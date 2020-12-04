0c5ddd16ef75475a01e7e9b85e1db7354be24457
Amaia <amaia.berrocal@gmail.com>
2020-12-04 12:46:21 +0100

    Conexión a BBDD y creación de tabla
    tabla con campos inventados para ver cómo se hace

--- src/main/java/com/thermostate/db/SqulitoConnection.java
+++ src/main/java/com/thermostate/db/SqulitoConnection.java
@@ -4,8 +4,32 @@ import java.sql.*;
 
 public class SqulitoConnection {
     public static void main( String args[] ) {
-        Connection c = null;
+        Connection c = createConnection();
+        createTable(c);
+    }
 
+    private static void createTable(Connection c) {
+        Statement stmt = null;
+        try {
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE COMPANY " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " AGE            INT     NOT NULL, " +
+                    " ADDRESS        CHAR(50), " +
+                    " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table created successfully");
+    }
+
+    public static Connection createConnection() {
+        Connection c = null;
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:squlito/test.db");
@@ -14,5 +38,6 @@ public class SqulitoConnection {
             System.exit(0);
         }
         System.out.println("Opened database successfully");
+        return c;
     }
 }
