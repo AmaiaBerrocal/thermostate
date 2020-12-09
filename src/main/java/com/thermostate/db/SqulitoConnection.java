1b496a7fa9f2ff8227a1b37cd0ffb7be65e83a3a
Amaia <amaia.berrocal@gmail.com>
2020-12-09 13:41:34 +0100

    Inserción de usuarios en BBDD

--- src/main/java/com/thermostate/db/SqulitoConnection.java
+++ src/main/java/com/thermostate/db/SqulitoConnection.java
@@ -4,28 +4,9 @@ import java.sql.*;
 
 public class SqulitoConnection {
     public static void main( String args[] ) {
-        Connection c = createConnection();
-        createTable(c);
-    }
-
-    private static void createTable(Connection c) {
-        Statement stmt = null;
-        try {
-            stmt = c.createStatement();
-            String sql = "CREATE TABLE COMPANY " +
-                    "(ID INT PRIMARY KEY     NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " AGE            INT     NOT NULL, " +
-                    " ADDRESS        CHAR(50), " +
-                    " SALARY         REAL)";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
-        System.out.println("Table created successfully");
+        createTable();
+        insertUser("AMAIA", "OLIHFWOE");
+        insertUser("INIGO", "LKONSEDJNFCD");
     }
 
     public static Connection createConnection() {
@@ -35,9 +16,38 @@ public class SqulitoConnection {
             c = DriverManager.getConnection("jdbc:sqlite:squlito/test.db");
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
         }
         System.out.println("Opened database successfully");
         return c;
+    }
+
+    private static void createTable() {
+        try(Connection c = createConnection();
+            Statement stmt = c.createStatement()) {
+
+            String sql = "CREATE TABLE USERS" +
+                    "(ID INTEGER PRIMARY KEY," +
+                    " NAME TEXT NOT NULL, " +
+                    " PASSWORD TEXT NOT NULL, " +
+                    " SALT TEXT)";
+            stmt.executeUpdate(sql);
+
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+        System.out.println("Table created successfully");
+    }
+
+    public static void insertUser(String name, String password) {
+        try(Connection c = createConnection();
+            PreparedStatement stmt = c.prepareStatement("INSERT INTO USERS (NAME,PASSWORD,SALT) VALUES (?, ?, null);")) {
+            stmt.setString(1, name);
+            stmt.setString(2, password);
+            stmt.executeUpdate();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
     }
 }
