184bdecaf880f664b15c1b964fd50e6d23a6f43f
Amaia <amaia.berrocal@gmail.com>
2020-12-30 13:25:46 +0100

    Cambio doGet por doPost y devuelve HTTP status en vez de redireccionar

--- src/main/java/com/thermostate/infrastructure/Validation.java
+++ src/main/java/com/thermostate/infrastructure/Validation.java
@@ -11,14 +11,19 @@ import java.io.IOException;
 @WebServlet("/validation")
 public class Validation extends HttpServlet {
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String name = req.getParameter("name");
-        String password = req.getParameter("password");
-        Users users = new Users(new SqulitoConnection());
-        if (users.isValidPassword(name, password)){
-            resp.sendRedirect("/vacia.html");
-        } else {
-            resp.sendRedirect("/login.jsp?error=true");
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            String name = req.getParameter("name");
+            String password = req.getParameter("password");
+            Users users = new Users(new SqulitoConnection());
+            if (users.isValidPassword(name, password)){
+                return;
+            } else {
+                resp.setStatus(403);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500);
         }
     }
 }
