0d6c732f744ce34694d18f1c8940a5de735429e3
Amaia Berrocal <amaia.berrocal@gmail.com>
2022-06-09 15:40:37 +0200

    #21 java project created

---
+++ src/test/java/com/amaia/thermostate/ThermostateApplicationTests.java
@@ -0,0 +1,14 @@
+package com.amaia.thermostate;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ThermostateApplicationTests {
+
+	@Test
+	public void testAdd() {
+		assertEquals(42, Integer.sum(19, 23));
+	}
+}
