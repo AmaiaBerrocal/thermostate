468732a1b89af97f7ba2934994dd398ece18c6eb
Amaia Berrocal <amaia.berrocal@gmail.com>
2022-09-29 15:44:00 +0200

    #11 Added properties loader

--- src/main/java/com/thermostate/infrastructure/db/DbUtils.java
+++ src/main/java/com/thermostate/infrastructure/db/DbUtils.java
@@ -1,19 +1,23 @@
 package com.thermostate.infrastructure.db;
 
+import com.thermostate.infrastructure.properties.PropertiesLoader;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
+@Component
+@AllArgsConstructor
 public class DbUtils {
-    //url siempre es jdbc:subprotocol:subname donde subname es la ruta y nombre de mi archivo
-    final String URL = "jdbc:sqlite:./db/thermostate.db";
+    final PropertiesLoader properties;
 
     private Connection createConnection() {
         Connection con = null;
         try{
-            con = DriverManager.getConnection(URL);
+            con = DriverManager.getConnection(properties.getUrl());
             if (con == null){
                 throw new RuntimeException("Not able to connect to database.");
             }
