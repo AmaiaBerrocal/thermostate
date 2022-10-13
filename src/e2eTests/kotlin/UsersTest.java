f6ea64dce6b68db52e1052af07e18ec05701c0bc
Amaia Berrocal <amaia.berrocal@gmail.com>
2022-10-13 13:09:55 +0200

    #11 Fix e2e tests

--- src/e2eTests/kotlin/UsersTest.java
+++ src/e2eTests/kotlin/UsersTest.java
@@ -1,4 +1,5 @@
-import http.Petition;
+import db.E2EDB;
+import http.E2ERequest;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -7,10 +8,26 @@ class UsersTest {
 
     @Test
     void should_create_a_user() {
-        Petition
+        E2EDB e2edb = new E2EDB("jdbc:sqlite:./db/thermostate.db");
+        e2edb.givenEmptyTable("USERS");
+
+        E2ERequest
                 .to("http://localhost:8080/user")
                 .withContentType("application/json;charset=UTF-8")
                 .sendAPost(Map.of("name", "Amaia", "password", "pass", "email", "lala@gmail.com"))
                 .assertThatResponseIsOk();
+        e2edb
+                .doQuery("SELECT * FROM USERS WHERE NAME = 'Amaia'")
+                .assertThatExistAnEntryWithFields(Map.of("email", "lala@gmail.com"));
+    }
+
+    @Test
+    void should_fail_create_a_user_with_incorrect_dates() {
+        E2ERequest
+                .to("http://localhost:8080/user")
+                .withContentType("application/json;charset=UTF-8")
+                .sendAPost(Map.of("name", "", "password", "pass", "email", "lala@gmail.com"))
+                .assertThatResponseCodeIs(400);
+
     }
 }
