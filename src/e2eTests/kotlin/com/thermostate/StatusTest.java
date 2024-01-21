5c36db13a66558767d47c22d49aa5c1fcc11c35c
inigodm <inigo.delgado@gmail.com>
2024-01-21 01:36:09 +0100

    #44 Some tests

---
+++ src/e2eTests/kotlin/com/thermostate/StatusTest.java
@@ -0,0 +1,40 @@
+package com.thermostate;
+
+import com.thermostate.brain.domain.ThermostateStatus;
+import db.E2EDB;
+import http.E2ERequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static com.thermostate.shared.HttpRequestsUtils.createSingleUser;
+import static com.thermostate.shared.HttpRequestsUtils.getBearer;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StatusTest {
+    E2EDB e2edb;
+    @BeforeEach
+    public void setup() {
+        e2edb = new E2EDB("jdbc:sqlite:./assets/thermostate.db");
+        e2edb.givenEmptyTable("SCHEDULES");
+        e2edb.givenEmptyTable("USERS");
+        createSingleUser(e2edb);
+    }
+
+    @Test
+    public void should_return_status() {
+        var response = E2ERequest
+                .to("http://localhost:8080/status")
+                .withHeader("Authorization", getBearer())
+                .withContentType("application/json;charset=UTF-8")
+                .sendAGet(Map.of())
+                .assertThatResponseIsOk();
+
+        var map = response.body(); // TODO: fix this
+        assertThat(((Map<String, Object>)map.get("targetTemperature")).get("temp")).isEqualTo(1600.0);
+        assertThat(((Map<String, Object>)map.get("currentTemperature")).get("temp")).isEqualTo(1234.0);
+        assertThat(((Map<String, Object>)map.get("externalTemperature")).get("temp")).isEqualTo(6.0);
+        assertThat((map.get("active"))).isEqualTo(true);
+    }
+}
