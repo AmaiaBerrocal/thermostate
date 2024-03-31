d59b03e8880b63fee83e81242913444e8ffebe68
inigodm <inigo.delgado@gmail.com>
2024-03-31 16:34:21 +0200

    #46 fixed gradle

--- src/main/java/com/thermostate/eroski/infrastructure/LoadFromFile.kt
+++ src/main/java/com/thermostate/eroski/infrastructure/LoadFromFile.kt
@@ -3,7 +3,9 @@ package com.thermostate.eroski.infrastructure
 import com.thermostate.eroski.domain.DataLoader
 import org.apache.pdfbox.Loader
 import org.apache.pdfbox.text.PDFTextStripper
+import org.springframework.stereotype.Component
 
+@Component
 class LoadFromFile : DataLoader {
     override fun load(filePath: String): String {
         val document = Loader.loadPDF(java.io.File(filePath))
