393b126cc79fa05cb26fbaa2ed412245208d04bf
inigodm <inigo.delgado@gmail.com>
2025-03-06 23:41:32 +0100

    pdf3

--- src/main/java/com/thermostate/eroski/infrastructure/Items.kt
+++ src/main/java/com/thermostate/eroski/infrastructure/Items.kt
@@ -3,5 +3,4 @@ package com.thermostate.eroski.infrastructure
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-interface Items : JpaRepository<ItemJpa, UUID> {
-}
+interface Items : JpaRepository<ItemJpa, UUID>
